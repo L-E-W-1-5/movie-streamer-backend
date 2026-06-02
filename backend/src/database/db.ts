b032b80064659f9932559023500cc9fc73086846
L-E-W-1-5 <lewiswootton88@hotmail.com>
2026-06-02 21:01:26 +0100

    post database migration

--- backend/src/database/db.ts
+++ backend/src/database/db.ts
@@ -1,11 +1,18 @@
 import { Pool } from 'pg'
 
+// const pool = new Pool({
+//     host: process.env.RDS_HOST,
+//     port: parseInt(process.env.RDS_PORT || '5432'),
+//     database: process.env.RDS_NAME,
+//     user: process.env.RDS_USER,
+//     password: process.env.RDS_PASSWORD
+// })
+
 const pool = new Pool({
-    host: process.env.RDS_HOST,
-    port: parseInt(process.env.RDS_PORT || '5432'),
-    database: process.env.RDS_NAME,
-    user: process.env.RDS_USER,
-    password: process.env.RDS_PASSWORD
+  connectionString: process.env.NEON_DB,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
 })
 
 export default pool
